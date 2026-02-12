305692d579de22523c318ed1127a63ca2fa1f2e0
siddheshghule-cmy <siddhesh.ghule@sysfort.com>
2026-02-12 17:49:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,43 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const pool = new Pool({
-     host: process.env.DB_HOST,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     port: process.env.DB_PORT,
-     database: process.env.DB_NAME
-   });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+const db = {
+  all: (query, params, callback) => {
+    pool.query(query, params, (err, result) => {
+      if (err) {
+        return callback(err, null);
+      }
+      callback(null, result.rows);
+    });
+  },
+  
+  run: (query, params, callback) => {
+    pool.query(query, params, (err, result) => {
+      if (err) {
+        return callback(err, null);
+      }
+      callback(null, result);
+    });
+  },
+
+  get: (query, params, callback) => {
+    pool.query(query, params, (err, result) => {
+      if (err) {
+        return callback(err, null);
+      }
+      callback(null, result.rows[0]);
+    });
+  },
+};
 
 export default db;
-
